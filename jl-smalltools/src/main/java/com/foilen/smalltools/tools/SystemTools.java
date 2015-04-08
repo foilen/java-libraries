f2664d82218dce6515c59a8b74b4956bc4a93ec9
Simon Levesque <simon@simonlevesque.com>
2015-04-08 07:17:45 -0400

    Add a systemtools to set properties if they are not defined

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/tools/SystemTools.java
@@ -0,0 +1,31 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+public class SystemTools {
+
+    /**
+     * Sets a system property if not already defined.
+     * 
+     * @param key
+     *            the property's key
+     * @param value
+     *            the property's value
+     */
+    public static void setPropertyIfNotSet(String key, String value) {
+        String currentValue = System.getProperty(key);
+        if (currentValue == null) {
+            System.setProperty(key, value);
+        }
+    }
+
+    private SystemTools() {
+    }
+
+}
