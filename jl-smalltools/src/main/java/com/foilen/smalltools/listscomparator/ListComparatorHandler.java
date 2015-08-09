e7212297e734cc80edc78e883c464d3b9ff6b379
Simon Levesque <simon@simonlevesque.com>
2015-08-08 20:49:45 -0400

    Move list comparaison out of the incubator. Add ListComparatorHandler

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/listscomparator/ListComparatorHandler.java
@@ -0,0 +1,47 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.listscomparator;
+
+/**
+ * Extend with what to do with what is different and what is the same.
+ * 
+ * @param <L>
+ *            type of the left item
+ * @param <R>
+ *            type of the right item
+ */
+public interface ListComparatorHandler<L, R> {
+
+    /**
+     * What to do with items present in both lists.
+     * 
+     * @param left
+     *            the left item
+     * @param right
+     *            the right item
+     */
+    void both(L left, R right);
+
+    /**
+     * What to do with items present only in the left side.
+     * 
+     * @param left
+     *            the left item
+     */
+    void leftOnly(L left);
+
+    /**
+     * What to do with items present only in the right side.
+     * 
+     * @param right
+     *            the right item
+     */
+    void rightOnly(R right);
+
+}
--- jl-smalltools/src/main/java/com/foilen/smalltools/listscomparator/ListsComparator.java
+++ jl-smalltools/src/main/java/com/foilen/smalltools/listscomparator/ListsComparator.java
@@ -28,10 +28,42 @@ public final class ListsComparator {
      * @return the comparison
      */
     public static <T extends Comparable<T>> List<ListsComparatorDifference<T>> compareLists(List<T> lefts, List<T> rights) {
+        final List<ListsComparatorDifference<T>> comparisons = new ArrayList<ListsComparatorDifference<T>>();
+
+        compareLists(lefts, rights, new ListComparatorHandler<T, T>() {
+            @Override
+            public void both(T left, T right) {
+            }
+
+            @Override
+            public void leftOnly(T left) {
+                comparisons.add(new ListsComparatorDifference<T>(left, -1));
+            }
+
+            @Override
+            public void rightOnly(T right) {
+                comparisons.add(new ListsComparatorDifference<T>(right, 1));
+            }
+        });
+
+        return comparisons;
+    }
+
+    /**
+     * Compare two ordered lists for differences.
+     * 
+     * @param lefts
+     *            ordered list
+     * @param rights
+     *            ordered list
+     * @param listComparatorHandler
+     *            the change handler
+     * @param <T>
+     *            the type of the items
+     */
+    public static <T extends Comparable<T>> void compareLists(List<T> lefts, List<T> rights, ListComparatorHandler<T, T> listComparatorHandler) {
         int posLeft = 0;
         int posRight = 0;
-
-        List<ListsComparatorDifference<T>> comparisons = new ArrayList<ListsComparatorDifference<T>>();
 
         while (posLeft < lefts.size() || posRight < rights.size()) {
             T left = null;
@@ -46,13 +78,13 @@ public final class ListsComparator {
             // If one is null
             if (left == null) {
                 ++posRight;
-                comparisons.add(new ListsComparatorDifference<T>(right, 1));
+                listComparatorHandler.rightOnly(right);
                 continue;
             }
 
             if (right == null) {
                 ++posLeft;
-                comparisons.add(new ListsComparatorDifference<T>(left, -1));
+                listComparatorHandler.leftOnly(left);
                 continue;
             }
 
@@ -60,17 +92,17 @@ public final class ListsComparator {
             int comparison = left.compareTo(right);
             if (comparison < 0) {
                 ++posLeft;
-                comparisons.add(new ListsComparatorDifference<T>(left, -1));
+                listComparatorHandler.leftOnly(left);
             } else if (comparison > 0) {
                 ++posRight;
-                comparisons.add(new ListsComparatorDifference<T>(right, 1));
+                listComparatorHandler.rightOnly(right);
             } else {
+                listComparatorHandler.both(left, right);
                 ++posLeft;
                 ++posRight;
             }
         }
 
-        return comparisons;
     }
 
     private ListsComparator() {
