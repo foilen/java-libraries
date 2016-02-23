3c1986f2095277f44b17cb6092f71dda6e0f3343
Simon Levesque <simon@simonlevesque.com>
2016-02-23 16:56:08 -0500

    Add SingleDependencyResolverTools

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/tools/SingleDependencyResolverTools.java
@@ -0,0 +1,119 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+
+/**
+ * To get an execution path that works with the provided dependencies. Supports only one dependency per item and will throw an exception if more than one dependency are specified and if it is
+ * circular.
+ * 
+ * <pre>
+ * Sample usage:
+ * A -> B -> C
+ *   -> D -> E
+ *   
+ * SingleDependencyResolverTools resolver = new SingleDependencyResolverTools();
+ * resolver.addDependency("E", "D");
+ * resolver.addDependency("B", "A");
+ * resolver.addDependency("C", "B");
+ * resolver.addDependency("D", "A");
+ *
+ * Lresolver.getExecution(); // "A", "B", "C", "D", "E"
+ * 
+ * </pre>
+ */
+public class SingleDependencyResolverTools {
+
+    private class Item {
+        public String name;
+        public List<Item> dependedBy = new ArrayList<>();
+    }
+
+    private Map<String, Item> itemByName = new HashMap<>();
+    private List<Item> roots = new ArrayList<>();
+    private List<String> hasDependency = new ArrayList<>();
+
+    /**
+     * Add a dependency.
+     * 
+     * @param item
+     *            the item that depends on another one
+     * @param dependsOnItem
+     *            the item it depends on (could be null or empty to specify that it depends on nothing)
+     * @return this
+     * @throws SmallToolsException
+     *             if "item" was already provided
+     */
+    public SingleDependencyResolverTools addDependency(String item, String dependsOnItem) {
+        if (hasDependency.contains(item)) {
+            throw new SmallToolsException("Item " + item + " already has one dependency");
+        }
+        hasDependency.add(item);
+
+        // Get the item it depends on
+        Item depended = itemByName.get(dependsOnItem);
+        if (depended == null) {
+            depended = new Item();
+            depended.name = dependsOnItem;
+            itemByName.put(dependsOnItem, depended);
+            roots.add(depended);
+        }
+
+        // Get the item
+        Item currentItem = itemByName.get(item);
+        if (currentItem == null) {
+            currentItem = new Item();
+            currentItem.name = item;
+            itemByName.put(item, currentItem);
+        } else {
+            roots.remove(currentItem);
+        }
+
+        // Add it
+        depended.dependedBy.add(currentItem);
+
+        return this;
+    }
+
+    /**
+     * To retrieve the execution plan.
+     * 
+     * @return the excution plan
+     * @throws SmallToolsException
+     *             if it is a circular dependency
+     */
+    public List<String> getExecution() {
+
+        List<String> executionPlan = new ArrayList<>();
+        for (Item item : roots) {
+            visit(executionPlan, item);
+        }
+
+        // Check if circular
+        if (executionPlan.size() != itemByName.size()) {
+            throw new SmallToolsException("Has a circular dependency");
+        }
+
+        return executionPlan;
+    }
+
+    private void visit(List<String> executionPlan, Item item) {
+        executionPlan.add(item.name);
+        for (Item child : item.dependedBy) {
+            visit(executionPlan, child);
+        }
+    }
+
+}
---
+++ jl-smalltools/src/test/java/com/foilen/smalltools/tools/SingleDependencyResolverToolsTest.java
@@ -0,0 +1,82 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+
+public class SingleDependencyResolverToolsTest {
+
+    /**
+     * <pre>
+     * A -> B -> A
+     * </pre>
+     */
+    @Test(expected = SmallToolsException.class)
+    public void testCircular() {
+        SingleDependencyResolverTools resolver = new SingleDependencyResolverTools();
+        resolver.addDependency("B", "A");
+        resolver.addDependency("A", "B");
+
+        resolver.getExecution();
+    }
+
+    /**
+     * <pre>
+     * A -> B -> C -> D -> A
+     * E -> F
+     * </pre>
+     */
+    @Test(expected = SmallToolsException.class)
+    public void testCircular2() {
+        SingleDependencyResolverTools resolver = new SingleDependencyResolverTools();
+        resolver.addDependency("B", "A");
+        resolver.addDependency("C", "B");
+        resolver.addDependency("D", "C");
+        resolver.addDependency("A", "D");
+        resolver.addDependency("F", "E");
+
+        resolver.getExecution();
+    }
+
+    /**
+     * <pre>
+     * A -> B -> C
+     *   -> D -> E
+     * </pre>
+     */
+    @Test
+    public void testSuccess() {
+        SingleDependencyResolverTools resolver = new SingleDependencyResolverTools();
+        resolver.addDependency("E", "D");
+        resolver.addDependency("B", "A");
+        resolver.addDependency("C", "B");
+        resolver.addDependency("D", "A");
+
+        List<String> executionPlan = resolver.getExecution();
+
+        Assert.assertEquals(Arrays.asList("A", "B", "C", "D", "E"), executionPlan);
+    }
+
+    /**
+     * A,B -> C
+     */
+    @Test(expected = SmallToolsException.class)
+    public void testTooManyDependencies() {
+        SingleDependencyResolverTools resolver = new SingleDependencyResolverTools();
+        resolver.addDependency("C", "A");
+        resolver.addDependency("C", "B");
+    }
+
+}
