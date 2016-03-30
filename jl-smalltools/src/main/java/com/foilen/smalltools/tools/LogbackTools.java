b44abdecb147a78b852cb15e4af9d6092ac6b619
Simon Levesque <simon@simonlevesque.com>
2016-03-30 19:52:04 -0400

    Add a logback tool to change the config file

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/tools/LogbackTools.java
@@ -0,0 +1,49 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+/**
+ * Some tools for Logback.
+ * 
+ * <pre>
+ * Dependencies:
+ * compile 'ch.qos.logback:logback-classic:1.1.3'
+ * </pre>
+ */
+public class LogbackTools {
+
+    /**
+     * Reset the loggers with the new resource config.
+     * 
+     * @param configResourceName
+     *            the name of the config file
+     */
+    public static void changeConfig(String configResourceName) {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            context.reset();
+            configurator.doConfigure(LogbackTools.class.getResourceAsStream(configResourceName));
+        } catch (JoranException je) {
+            throw new RuntimeException(je);
+        }
+    }
+
+    private LogbackTools() {
+    }
+
+}
