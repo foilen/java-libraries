bad0d551550e6f48c00a394236c224a95eb7df7f
Simon Levesque <simon@simonlevesque.com>
2016-06-13 07:54:17 -0400

    SpaceConverterTool - convertToBytes

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/tools/SpaceConverterTool.java
@@ -0,0 +1,121 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+
+/**
+ * To help converting space values to Long.
+ */
+public class SpaceConverterTool {
+
+    static public long KB = 1000L;
+    static public long MB = 1000 * 1000L;
+    static public long GB = 1000 * 1000 * 1000L;
+    static public long TB = 1000 * 1000 * 1000 * 1000L;
+
+    static public long KIB = 1024L;
+    static public long MIB = 1024 * 1024L;
+    static public long GIB = 1024 * 1024 * 1024L;
+    static public long TIB = 1024 * 1024 * 1024 * 1024L;
+
+    /**
+     * Convert text like "15k", "15 k", "15 kB", "15 kb" to its {@link Long} representation (always bytes; not bits).
+     * 
+     * <ul>
+     * <li>Support "B", "kB", "mB", "gB" and "tB" (powers of 1000)</li>
+     * <li>Support "B", "kiB", "miB", "giB" and "tiB" (powers of 1024)</li>
+     * <li>It is case insensitive, that's why it will never give bits, but always bytes</li>
+     * </ul>
+     * 
+     * @param space
+     *            the text space
+     * @return the amount of bytes
+     */
+    static public long convertToBytes(String space) {
+        boolean inNum = true;
+        String spaceLower = space.toLowerCase();
+        String numeric = "";
+        String unit = "";
+        for (int i = 0; i < spaceLower.length(); ++i) {
+            char current = spaceLower.charAt(i);
+            if (inNum) {
+                // The numeric part
+                if ((current >= '0' && current <= '9') || current == '.') {
+                    numeric += current;
+                    continue;
+                } else {
+                    inNum = false;
+                }
+            }
+
+            // The unit part
+            // Skip spaces
+            if (current == '\t' || current == ' ') {
+                continue;
+            } else {
+                unit += current;
+            }
+
+        }
+
+        // Convert
+        try {
+            double value = Double.parseDouble(numeric);
+            switch (unit) {
+            case "":
+            case "b":
+                break;
+            case "k":
+            case "kb":
+                value *= KB;
+                break;
+            case "m":
+            case "mb":
+                value *= MB;
+                break;
+            case "g":
+            case "gb":
+                value *= GB;
+                break;
+            case "t":
+            case "tb":
+                value *= TB;
+                break;
+
+            case "ki":
+            case "kib":
+                value *= KIB;
+                break;
+            case "mi":
+            case "mib":
+                value *= MIB;
+                break;
+            case "gi":
+            case "gib":
+                value *= GIB;
+                break;
+            case "ti":
+            case "tib":
+                value *= TIB;
+                break;
+
+            default:
+                throw new SmallToolsException(space + " is an invalid space");
+            }
+            return (long) value;
+        } catch (SmallToolsException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new SmallToolsException(space + " is an invalid space");
+        }
+
+    }
+
+}
---
+++ jl-smalltools/src/test/java/com/foilen/smalltools/tools/SpaceConverterToolTest.java
@@ -0,0 +1,108 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class SpaceConverterToolTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testConvertBytes_allNumbers() {
+        Assert.assertEquals(1023456789000000L, SpaceConverterTool.convertToBytes("1023456789 m"));
+    }
+
+    @Test
+    public void testConvertBytes_bytes() {
+        Assert.assertEquals(85L, SpaceConverterTool.convertToBytes("85"));
+        Assert.assertEquals(85L, SpaceConverterTool.convertToBytes("85b"));
+        Assert.assertEquals(85L, SpaceConverterTool.convertToBytes("85B"));
+        Assert.assertEquals(85L, SpaceConverterTool.convertToBytes("85 B"));
+        Assert.assertEquals(85L, SpaceConverterTool.convertToBytes("85 b"));
+    }
+
+    @Test
+    public void testConvertBytes_gibibytes() {
+        Assert.assertEquals(91268055040L, SpaceConverterTool.convertToBytes("85gib"));
+        Assert.assertEquals(91268055040L, SpaceConverterTool.convertToBytes("85 giB"));
+    }
+
+    @Test
+    public void testConvertBytes_gigabytes() {
+        Assert.assertEquals(85000000000L, SpaceConverterTool.convertToBytes("85g"));
+        Assert.assertEquals(85000000000L, SpaceConverterTool.convertToBytes("85 g"));
+        Assert.assertEquals(85000000000L, SpaceConverterTool.convertToBytes("85 gB"));
+    }
+
+    @Test
+    public void testConvertBytes_invalid_char() {
+        thrown.expectMessage("% is an invalid space");
+        SpaceConverterTool.convertToBytes("%");
+    }
+
+    @Test
+    public void testConvertBytes_invalid_number() {
+        thrown.expectMessage("kb is an invalid space");
+        SpaceConverterTool.convertToBytes("kb");
+    }
+
+    @Test
+    public void testConvertBytes_invalid_unit() {
+        thrown.expectMessage("75J is an invalid space");
+        SpaceConverterTool.convertToBytes("75J");
+    }
+
+    @Test
+    public void testConvertBytes_kibibytes() {
+        Assert.assertEquals(87040L, SpaceConverterTool.convertToBytes("85kib"));
+        Assert.assertEquals(87040L, SpaceConverterTool.convertToBytes("85 kiB"));
+    }
+
+    @Test
+    public void testConvertBytes_kilobytes() {
+        Assert.assertEquals(85000L, SpaceConverterTool.convertToBytes("85k"));
+        Assert.assertEquals(85660L, SpaceConverterTool.convertToBytes("85.66k"));
+        Assert.assertEquals(85000L, SpaceConverterTool.convertToBytes("85 k"));
+        Assert.assertEquals(85000L, SpaceConverterTool.convertToBytes("85\t k"));
+        Assert.assertEquals(85000L, SpaceConverterTool.convertToBytes("85 kB"));
+    }
+
+    @Test
+    public void testConvertBytes_mebibytes() {
+        Assert.assertEquals(89128960L, SpaceConverterTool.convertToBytes("85mib"));
+        Assert.assertEquals(89128960L, SpaceConverterTool.convertToBytes("85 miB"));
+        Assert.assertEquals(89128960L, SpaceConverterTool.convertToBytes("85    miB   "));
+    }
+
+    @Test
+    public void testConvertBytes_megabytes() {
+        Assert.assertEquals(85000000L, SpaceConverterTool.convertToBytes("85m"));
+        Assert.assertEquals(85000000L, SpaceConverterTool.convertToBytes("85 m"));
+        Assert.assertEquals(85000000L, SpaceConverterTool.convertToBytes("85 mB"));
+    }
+
+    @Test
+    public void testConvertBytes_tebibytes() {
+        Assert.assertEquals(93458488360960L, SpaceConverterTool.convertToBytes("85tib"));
+        Assert.assertEquals(93458488360960L, SpaceConverterTool.convertToBytes("85 tiB"));
+    }
+
+    @Test
+    public void testConvertBytes_terabytes() {
+        Assert.assertEquals(85000000000000L, SpaceConverterTool.convertToBytes("85t"));
+        Assert.assertEquals(85000000000000L, SpaceConverterTool.convertToBytes("85 t"));
+        Assert.assertEquals(85000000000000L, SpaceConverterTool.convertToBytes("85 tB"));
+    }
+
+}
