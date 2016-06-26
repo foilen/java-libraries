fbb34c07cdb87c6265a1c7c79a245fa846747e43
Simon Levesque <simon@simonlevesque.com>
2016-06-26 15:44:14 -0400

    OneFileUpdateNotifyer - To track a single config file changes

---
+++ jl-incubator/src/main/java/com/foilen/smalltools/filesystemupdatewatcher/handler/OneFileUpdateNotifyer.java
@@ -0,0 +1,66 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.filesystemupdatewatcher.handler;
+
+import java.io.File;
+
+import com.foilen.smalltools.filesystemupdatewatcher.FileSystemUpdateBufferedHandler;
+import com.foilen.smalltools.filesystemupdatewatcher.FileSystemUpdateHandler;
+import com.foilen.smalltools.filesystemupdatewatcher.FileSystemUpdateWatcher;
+
+/**
+ * Use this class to track a file modification (e.g a config file that you want to reload when changed). If the file changes multiple times quickly, you will get a notification 2 seconds after the
+ * last change or max 10 seconds after the first change.
+ * 
+ * Don't forget to call {@link #initAutoUpdateSystem()} when you are ready to get the notifications
+ */
+public class OneFileUpdateNotifyer implements FileSystemUpdateHandler {
+
+    private String fileToWatch;
+    private File fileToWatchFile;
+    private OneFileUpdateNotifyerHandler handler;
+
+    public OneFileUpdateNotifyer(String fileToWatch, OneFileUpdateNotifyerHandler handler) {
+        this.fileToWatch = fileToWatch;
+        fileToWatchFile = new File(fileToWatch);
+        this.handler = handler;
+    }
+
+    @Override
+    public void created(File file) {
+        if (file.equals(fileToWatchFile)) {
+            handler.fileUpdated(fileToWatch);
+        }
+    }
+
+    @Override
+    public void deleted(File file) {
+        if (file.equals(fileToWatchFile)) {
+            handler.fileUpdated(fileToWatch);
+        }
+    }
+
+    /**
+     * Sends the first update event and start monitoring.
+     */
+    public void initAutoUpdateSystem() {
+        handler.fileUpdated(fileToWatch);
+        FileSystemUpdateWatcher fileSystemUpdateWatcher = new FileSystemUpdateWatcher(fileToWatchFile.getParentFile().getAbsolutePath());
+        fileSystemUpdateWatcher.addHandler(new FileSystemUpdateBufferedHandler(this, 2000, 10000));
+        fileSystemUpdateWatcher.init();
+    }
+
+    @Override
+    public void modified(File file) {
+        if (file.equals(fileToWatchFile)) {
+            handler.fileUpdated(fileToWatch);
+        }
+    }
+
+}
---
+++ jl-incubator/src/main/java/com/foilen/smalltools/filesystemupdatewatcher/handler/OneFileUpdateNotifyerHandler.java
@@ -0,0 +1,23 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.filesystemupdatewatcher.handler;
+
+public interface OneFileUpdateNotifyerHandler {
+
+    /**
+     * Implement this method that reloads the file when changed.
+     * 
+     * IMPORTANT: it must not throw any exception (e.g: consider no file as an empty file)
+     * 
+     * @param fileName
+     *            the full path of the file
+     */
+    void fileUpdated(String fileName);
+
+}
