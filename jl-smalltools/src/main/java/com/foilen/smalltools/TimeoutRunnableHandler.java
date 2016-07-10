2f92f7ac6b5585688ced520dddb1c067bd57350f
Simon Levesque <simon@simonlevesque.com>
2016-07-10 15:57:46 -0400

    Create TimeoutRunnableHandler

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/TimeoutRunnableHandler.java
@@ -0,0 +1,66 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+
+/**
+ * When you need to execute something and wrap it in a timeout, you can simply put that in a {@link Runnable} and give it to this object. It will send an interrupt to the Thread. It will retrow any
+ * exception that occurs.
+ */
+public class TimeoutRunnableHandler {
+
+    private long timeoutInMilliseconds;
+    private Runnable runnable;
+
+    private RuntimeException thrownException;
+
+    public TimeoutRunnableHandler(long timeoutInMilliseconds, Runnable runnable) {
+        this.timeoutInMilliseconds = timeoutInMilliseconds;
+        this.runnable = runnable;
+    }
+
+    /**
+     * Call this method to execute the runnable. This call is waiting for the end of the execution or the timeout to occur.
+     * 
+     * @return the result
+     * 
+     * @throws SmallToolsException
+     *             if the timeout occurs
+     */
+    public void run() {
+
+        // Start the thread
+        Thread callThread = new Thread(() -> {
+            try {
+                runnable.run();
+            } catch (RuntimeException e) {
+                thrownException = e;
+            }
+        } , "TimeoutHandler");
+        callThread.start();
+
+        // Wait for the thread to finish or timeout
+        try {
+            callThread.join(timeoutInMilliseconds);
+        } catch (InterruptedException e) {
+        }
+        if (callThread.isAlive()) {
+            callThread.interrupt();
+            throw new SmallToolsException("The call is still running and the timeout passed");
+        }
+
+        // Throw any exception that was thrown
+        if (thrownException != null) {
+            throw thrownException;
+        }
+
+    }
+
+}
---
+++ jl-smalltools/src/test/java/com/foilen/smalltools/TimeoutRunnableHandlerTest.java
@@ -0,0 +1,53 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.foilen.smalltools.tools.ThreadTools;
+
+public class TimeoutRunnableHandlerTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test(timeout = 10000)
+    public void testRun_FailedException() {
+
+        thrown.expectMessage("Got an exception");
+
+        new TimeoutRunnableHandler(5000, () -> {
+            throw new RuntimeException("Got an exception");
+        }).run();
+    }
+
+    @Test(timeout = 10000)
+    public void testRun_FailedTimedOut() throws Exception {
+
+        thrown.expectMessage("The call is still running and the timeout passed");
+
+        new TimeoutRunnableHandler(500, () -> {
+            ThreadTools.sleep(20000);
+        }).run();
+    }
+
+    @Test(timeout = 10000)
+    public void testRun_Success() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        new TimeoutRunnableHandler(5000, () -> {
+            latch.countDown();
+        }).run();
+        latch.await();
+    }
+
+}
