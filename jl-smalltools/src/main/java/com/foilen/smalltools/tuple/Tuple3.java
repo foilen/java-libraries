a87dd1834ae7c807118eefcb7758068fda0844c2
Simon Levesque <simon@simonlevesque.com>
2016-09-14 21:21:22 -0400

    Add Tuple3

---
+++ jl-smalltools/src/main/java/com/foilen/smalltools/tuple/Tuple3.java
@@ -0,0 +1,104 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tuple;
+
+/**
+ * A tuple of 3 values.
+ */
+public class Tuple3<A, B, C> {
+
+    private A a;
+    private B b;
+    private C c;
+
+    public Tuple3() {
+    }
+
+    public Tuple3(A a, B b, C c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tuple3 other = (Tuple3) obj;
+        if (a == null) {
+            if (other.a != null)
+                return false;
+        } else if (!a.equals(other.a))
+            return false;
+        if (b == null) {
+            if (other.b != null)
+                return false;
+        } else if (!b.equals(other.b))
+            return false;
+        if (c == null) {
+            if (other.c != null)
+                return false;
+        } else if (!c.equals(other.c))
+            return false;
+        return true;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public C getC() {
+        return c;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((a == null) ? 0 : a.hashCode());
+        result = prime * result + ((b == null) ? 0 : b.hashCode());
+        result = prime * result + ((c == null) ? 0 : c.hashCode());
+        return result;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+
+    public void setC(C c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Tuple3 [a=");
+        builder.append(a);
+        builder.append(", b=");
+        builder.append(b);
+        builder.append(", c=");
+        builder.append(c);
+        builder.append("]");
+        return builder.toString();
+    }
+
+}
