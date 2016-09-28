75a3b5d74e2351e2913e867dad6b7d03170039e7
Simon Levesque <simon@simonlevesque.com>
2016-09-28 16:17:43 -0400

    Hibernate4Tools - Add unit tests

---
+++ jl-smalltools/src/test/java/com/foilen/smalltools/tools/test/entities/MachineStatisticNetwork.java
@@ -0,0 +1,62 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools.test.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class MachineStatisticNetwork {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String interfaceName;
+
+    private long inBytes;
+    private long outBytes;
+
+    public MachineStatisticNetwork() {
+    }
+
+    public MachineStatisticNetwork(String interfaceName, long inBytes, long outBytes) {
+        this.interfaceName = interfaceName;
+        this.inBytes = inBytes;
+        this.outBytes = outBytes;
+    }
+
+    public long getInBytes() {
+        return inBytes;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public long getOutBytes() {
+        return outBytes;
+    }
+
+    public void setInBytes(long inBytes) {
+        this.inBytes = inBytes;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName;
+    }
+
+    public void setOutBytes(long outBytes) {
+        this.outBytes = outBytes;
+    }
+
+}
---
+++ jl-smalltools/src/test/java/com/foilen/smalltools/tools/test/entities/MachineStatistics.java
@@ -0,0 +1,175 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools.test.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class MachineStatistics {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "machine_id", nullable = false)
+    private Machine machine;
+
+    private Date timestamp;
+
+    private long cpuUsed;
+    private long cpuTotal;
+
+    private long memoryUsed;
+    private long memoryTotal;
+
+    private long memorySwapUsed;
+    private long memorySwapTotal;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<MachineStatisticFS> fs = new TreeSet<>();
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<MachineStatisticNetwork> networks = new TreeSet<>();
+
+    // Aggregation
+    private int aggregationsForHour = 0;
+    private int aggregationsForDay = 0;
+
+    public MachineStatistics() {
+    }
+
+    public MachineStatistics(Machine machine, Date timestamp, long cpuUsed, long cpuTotal, long memoryUsed, long memoryTotal, long memorySwapUsed, long memorySwapTotal) {
+        this.machine = machine;
+        this.timestamp = timestamp;
+        this.cpuUsed = cpuUsed;
+        this.cpuTotal = cpuTotal;
+        this.memoryUsed = memoryUsed;
+        this.memoryTotal = memoryTotal;
+        this.memorySwapUsed = memorySwapUsed;
+        this.memorySwapTotal = memorySwapTotal;
+    }
+
+    public int getAggregationsForDay() {
+        return aggregationsForDay;
+    }
+
+    public int getAggregationsForHour() {
+        return aggregationsForHour;
+    }
+
+    public long getCpuTotal() {
+        return cpuTotal;
+    }
+
+    public long getCpuUsed() {
+        return cpuUsed;
+    }
+
+    public Set<MachineStatisticFS> getFs() {
+        return fs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Machine getMachine() {
+        return machine;
+    }
+
+    public long getMemorySwapTotal() {
+        return memorySwapTotal;
+    }
+
+    public long getMemorySwapUsed() {
+        return memorySwapUsed;
+    }
+
+    public long getMemoryTotal() {
+        return memoryTotal;
+    }
+
+    public long getMemoryUsed() {
+        return memoryUsed;
+    }
+
+    public Set<MachineStatisticNetwork> getNetworks() {
+        return networks;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setAggregationsForDay(int aggregationsForDay) {
+        this.aggregationsForDay = aggregationsForDay;
+    }
+
+    public void setAggregationsForHour(int aggregationsForHour) {
+        this.aggregationsForHour = aggregationsForHour;
+    }
+
+    public void setCpuTotal(long cpuTotal) {
+        this.cpuTotal = cpuTotal;
+    }
+
+    public void setCpuUsed(long cpuUsed) {
+        this.cpuUsed = cpuUsed;
+    }
+
+    public void setFs(Set<MachineStatisticFS> fs) {
+        this.fs = fs;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
+
+    public void setMemorySwapTotal(long memorySwapTotal) {
+        this.memorySwapTotal = memorySwapTotal;
+    }
+
+    public void setMemorySwapUsed(long memorySwapUsed) {
+        this.memorySwapUsed = memorySwapUsed;
+    }
+
+    public void setMemoryTotal(long memoryTotal) {
+        this.memoryTotal = memoryTotal;
+    }
+
+    public void setMemoryUsed(long memoryUsed) {
+        this.memoryUsed = memoryUsed;
+    }
+
+    public void setNetworks(Set<MachineStatisticNetwork> networks) {
+        this.networks = networks;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
