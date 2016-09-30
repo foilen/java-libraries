846b1a18619426e986d3f98fba9edea21db3ada3
Simon Levesque <simon@simonlevesque.com>
2016-09-29 21:32:13 -0400

    Fix unit tests

---
+++ jl-smalltools/jl-smalltools-hibernate5/src/test/java/com/foilen/smalltools/tools/test/entities/Machine.java
@@ -0,0 +1,70 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools.test.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Machine implements java.io.Serializable {
+
+    private static final long serialVersionUID = 2015122201L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Version
+    private long version;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ElementCollection
+    private Set<String> ips;
+
+    public Machine() {
+    }
+
+    public Machine(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Set<String> getIps() {
+        return ips;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setIps(Set<String> ips) {
+        this.ips = ips;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ jl-smalltools/jl-smalltools-hibernate5/src/test/java/com/foilen/smalltools/tools/test/entities/MachineStatisticFS.java
@@ -0,0 +1,74 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+    
+ */
+package com.foilen.smalltools.tools.test.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class MachineStatisticFS {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(length = 2000)
+    private String path;
+    private boolean isRoot;
+
+    private long usedSpace;
+    private long totalSpace;
+
+    public MachineStatisticFS() {
+    }
+
+    public MachineStatisticFS(String path, boolean isRoot, long usedSpace, long totalSpace) {
+        this.path = path;
+        this.isRoot = isRoot;
+        this.usedSpace = usedSpace;
+        this.totalSpace = totalSpace;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public long getTotalSpace() {
+        return totalSpace;
+    }
+
+    public long getUsedSpace() {
+        return usedSpace;
+    }
+
+    public boolean isRoot() {
+        return isRoot;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setRoot(boolean isRoot) {
+        this.isRoot = isRoot;
+    }
+
+    public void setTotalSpace(long totalSpace) {
+        this.totalSpace = totalSpace;
+    }
+
+    public void setUsedSpace(long usedSpace) {
+        this.usedSpace = usedSpace;
+    }
+
+}
