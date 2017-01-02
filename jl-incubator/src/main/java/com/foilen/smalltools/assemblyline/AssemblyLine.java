a10c4560d309a04dfa8aafb931723313ca4dea0b
Simon Levesque <simon@simonlevesque.com>
2017-01-01 21:36:47 -0500

    Update copyright year

--- jl-incubator/src/main/java/com/foilen/smalltools/assemblyline/AssemblyLine.java
+++ jl-incubator/src/main/java/com/foilen/smalltools/assemblyline/AssemblyLine.java
@@ -1,10 +1,10 @@
 /*
     Java Libraries https://github.com/foilen/java-libraries
-    Copyright (c) 2015-2016 Foilen (http://foilen.com)
+    Copyright (c) 2015-2017 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
-    
+
  */
 package com.foilen.smalltools.assemblyline;
 
