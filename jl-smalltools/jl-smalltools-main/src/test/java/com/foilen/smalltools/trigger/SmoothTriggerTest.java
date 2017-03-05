7282a62276efbc6696c4b9c476632cf59b876c1f
Simon Levesque <simon@simonlevesque.com>
2017-03-05 08:29:02 -0500

    Format

--- jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/trigger/SmoothTriggerTest.java
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/trigger/SmoothTriggerTest.java
@@ -46,6 +46,69 @@ public class SmoothTriggerTest {
     }
 
     @Test(timeout = 7000)
+    public void testPassthru_Warmup_Max_Cancel() {
+        SmoothTrigger smoothTrigger = new SmoothTrigger(action) //
+                .setDelayAfterLastTriggerMs(DELAY) //
+                .setMaxDelayAfterFirstRequestMs(MAX_DELAY) //
+                .setFirstPassThrough(true) //
+                .start();
+
+        // Trigger first and at max
+        smoothTrigger.request(); // [TRIGGERED] At 0D ; next min delta 1D ; next max 3D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 0.5D ; next min delta 1.5D ; next max 3D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 1D ; next min delta 2D ; next max 3D
+
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 1.5D ; next min delta 2.5D ; next max 3D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 2D ; next min delta 3D ; next max 3D
+
+        ThreadTools.sleep(DELAY); // [TRIGGERED] At 3D by max while waiting . End of cooldown at 4D
+
+        // During cooldown, don't do right away
+        smoothTrigger.request(); // [NO] At 3D ; next min delta 4D ; next max 6D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 3.5D ; next min delta 4.5D ; next max 6D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 4D ; next min delta 5D ; next max 6D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 4.5D ; next min delta 5.5D ; next max 6D
+        ThreadTools.sleep(HALF_DELAY);
+        smoothTrigger.request(); // [NO] At 5D ; next min delta 6D ; next max 6D
+
+        ThreadTools.sleep(2 * DELAY + HALF_DELAY); // [TRIGGERED] At 6D by max while waiting . End of cooldown at 7D
+
+        // Wait end of cooldown, then 2 quickly -> Trigger first, wait delay for second
+        smoothTrigger.request(); // [TRIGGERED] At 7.5D ; next min delta 8.5D ; next max 10.5D
+        ThreadTools.sleep(QUICK);
+        smoothTrigger.request(); // [NO] At 7.5D ; next min delta 8.5D ; next max 10.5D
+
+        ThreadTools.sleep(2 * DELAY + HALF_DELAY); // [TRIGGERED] At 8.5D by delay while waiting . End of cooldown at 9.5D
+
+        // Wait end of cooldown, 1 [TRIGGERED], cancel, 1 [NO] at 11D
+        smoothTrigger.request(); // [TRIGGERED] At 10D ; next min delta 11D ; next max 13D
+        ThreadTools.sleep(QUICK);
+        smoothTrigger.cancelPending();
+        ThreadTools.sleep(QUICK);
+        smoothTrigger.request(); // [NO] At 10D ; next min delta 11D ; next max 13D
+
+        ThreadTools.sleep(2 * DELAY); // [TRIGGERED] At 11D by delay while waiting . End of cooldown at 12D
+
+        smoothTrigger.stop(true);
+
+        AssertTools.assertEqualsDelta(0, triggers.get(0), HALF_DELAY);
+        AssertTools.assertEqualsDelta(3 * DELAY, triggers.get(1), HALF_DELAY);
+        AssertTools.assertEqualsDelta(6 * DELAY, triggers.get(2), HALF_DELAY);
+        AssertTools.assertEqualsDelta(7 * DELAY + HALF_DELAY, triggers.get(3), HALF_DELAY);
+        AssertTools.assertEqualsDelta(8 * DELAY + HALF_DELAY, triggers.get(4), HALF_DELAY);
+        AssertTools.assertEqualsDelta(10 * DELAY, triggers.get(5), HALF_DELAY);
+        AssertTools.assertEqualsDelta(11 * DELAY, triggers.get(6), DELAY);
+        Assert.assertEquals(7, triggers.size());
+    }
+
+    @Test(timeout = 7000)
     public void testWarmup() {
         SmoothTrigger smoothTrigger = new SmoothTrigger(action) //
                 .setDelayAfterLastTriggerMs(DELAY) //
@@ -121,69 +184,6 @@ public class SmoothTriggerTest {
         AssertTools.assertEqualsDelta(MAX_DELAY, triggers.get(0), HALF_DELAY);
         AssertTools.assertEqualsDelta(5 * DELAY, triggers.get(1), HALF_DELAY);
 
-    }
-
-    @Test(timeout = 7000)
-    public void testPassthru_Warmup_Max_Cancel() {
-        SmoothTrigger smoothTrigger = new SmoothTrigger(action) //
-                .setDelayAfterLastTriggerMs(DELAY) //
-                .setMaxDelayAfterFirstRequestMs(MAX_DELAY) //
-                .setFirstPassThrough(true) //
-                .start();
-
-        // Trigger first and at max
-        smoothTrigger.request(); // [TRIGGERED] At 0D ; next min delta 1D ; next max 3D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 0.5D ; next min delta 1.5D ; next max 3D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 1D ; next min delta 2D ; next max 3D
-
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 1.5D ; next min delta 2.5D ; next max 3D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 2D ; next min delta 3D ; next max 3D
-
-        ThreadTools.sleep(DELAY); // [TRIGGERED] At 3D by max while waiting . End of cooldown at 4D
-
-        // During cooldown, don't do right away
-        smoothTrigger.request(); // [NO] At 3D ; next min delta 4D ; next max 6D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 3.5D ; next min delta 4.5D ; next max 6D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 4D ; next min delta 5D ; next max 6D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 4.5D ; next min delta 5.5D ; next max 6D
-        ThreadTools.sleep(HALF_DELAY);
-        smoothTrigger.request(); // [NO] At 5D ; next min delta 6D ; next max 6D
-
-        ThreadTools.sleep(2 * DELAY + HALF_DELAY); // [TRIGGERED] At 6D by max while waiting . End of cooldown at 7D
-
-        // Wait end of cooldown, then 2 quickly -> Trigger first, wait delay for second
-        smoothTrigger.request(); // [TRIGGERED] At 7.5D ; next min delta 8.5D ; next max 10.5D
-        ThreadTools.sleep(QUICK);
-        smoothTrigger.request(); // [NO] At 7.5D ; next min delta 8.5D ; next max 10.5D
-
-        ThreadTools.sleep(2 * DELAY + HALF_DELAY); // [TRIGGERED] At 8.5D by delay while waiting . End of cooldown at 9.5D
-
-        // Wait end of cooldown, 1 [TRIGGERED], cancel, 1 [NO] at 11D
-        smoothTrigger.request(); // [TRIGGERED] At 10D ; next min delta 11D ; next max 13D
-        ThreadTools.sleep(QUICK);
-        smoothTrigger.cancelPending();
-        ThreadTools.sleep(QUICK);
-        smoothTrigger.request(); // [NO] At 10D ; next min delta 11D ; next max 13D
-
-        ThreadTools.sleep(2 * DELAY); // [TRIGGERED] At 11D by delay while waiting . End of cooldown at 12D
-
-        smoothTrigger.stop(true);
-
-        AssertTools.assertEqualsDelta(0, triggers.get(0), HALF_DELAY);
-        AssertTools.assertEqualsDelta(3 * DELAY, triggers.get(1), HALF_DELAY);
-        AssertTools.assertEqualsDelta(6 * DELAY, triggers.get(2), HALF_DELAY);
-        AssertTools.assertEqualsDelta(7 * DELAY + HALF_DELAY, triggers.get(3), HALF_DELAY);
-        AssertTools.assertEqualsDelta(8 * DELAY + HALF_DELAY, triggers.get(4), HALF_DELAY);
-        AssertTools.assertEqualsDelta(10 * DELAY, triggers.get(5), HALF_DELAY);
-        AssertTools.assertEqualsDelta(11 * DELAY, triggers.get(6), DELAY);
-        Assert.assertEquals(7, triggers.size());
     }
 
 }
