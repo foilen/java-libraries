8cc32182370b8a6ebb7e64a078b7264cdaf41d53
Simon Levesque <simon@simonlevesque.com>
2017-09-18 20:09:59 -0400

    BeanPropertiesCopierTools

---
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/reflection/BeanPropertiesCopierTools.java
@@ -0,0 +1,280 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2017 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.reflection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+import com.foilen.smalltools.tools.AssertTools;
+
+/**
+ * Helps copying properties from one bean to another bean. It can also update a collection's items on place. See {@link #updateCollection(String)}.
+ *
+ * <pre>
+ * Employee employee = ...;
+ * EmployeeTo employeeTo = new EmployeeTo();
+ *
+ * BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(employee, employeeTo);
+ *
+ * // Same property name
+ * copierTools.copyProperty("firstName");
+ * copierTools.copyProperty("LastName");
+ *
+ * //
+ * copierTools.copyProperty("LastName");
+ *
+ * // Different property name
+ * copierTools.copyProperty("address", "homeAddress");
+ * </pre>
+ *
+ * <pre>
+ * Dependencies:
+ * compile 'org.apache.commons:commons-lang3:3.6'
+ * compile 'org.slf4j:slf4j-api:1.7.25'
+ * compile 'org.springframework:spring-orm:4.3.11.RELEASE'
+ * </pre>
+ */
+public class BeanPropertiesCopierTools {
+
+    private BeanWrapper sourceWrapper;
+    private BeanWrapper destinationWrapper;
+
+    /**
+     * Provide the bean wrappers.
+     *
+     * @param sourceWrapper
+     *            the source
+     * @param destinationWrapper
+     *            the destination
+     */
+    public BeanPropertiesCopierTools(BeanWrapper sourceWrapper, BeanWrapper destinationWrapper) {
+        AssertTools.assertNotNull(sourceWrapper, "The sourceWrapper cannot be null");
+        AssertTools.assertNotNull(sourceWrapper.getWrappedInstance(), "The source cannot be null");
+        AssertTools.assertNotNull(destinationWrapper, "The destinationWrapper cannot be null");
+        AssertTools.assertNotNull(destinationWrapper.getWrappedInstance(), "The destination cannot be null");
+
+        this.sourceWrapper = sourceWrapper;
+        this.destinationWrapper = destinationWrapper;
+    }
+
+    /**
+     * Provide the source object and the destination class to instantiate. You can then retrieve the destination with {@link #getDestination()}.
+     *
+     * @param source
+     *            the source
+     * @param destinationClass
+     *            the class to instantiate for the destination
+     */
+    public BeanPropertiesCopierTools(Object source, Class<?> destinationClass) {
+        AssertTools.assertNotNull(destinationClass, "The destinationClass cannot be null");
+        Object destination = ReflectionTools.instantiate(destinationClass);
+        init(source, destination);
+    }
+
+    /**
+     * Provide the objects.
+     *
+     * @param source
+     *            the source
+     * @param destination
+     *            the destination
+     */
+    public BeanPropertiesCopierTools(Object source, Object destination) {
+        init(source, destination);
+    }
+
+    /**
+     * Copy the property.
+     *
+     * @param propertyName
+     *            the name of the source's and destination's property
+     * @return this
+     */
+    public BeanPropertiesCopierTools copyProperty(String propertyName) {
+        return copyProperty(propertyName, propertyName);
+    }
+
+    /**
+     * Copy the property.
+     *
+     * @param sourcePropertyName
+     *            the name of the source's property
+     * @param destinationPropertyName
+     *            the name of the destination's property
+     * @return this
+     */
+    public BeanPropertiesCopierTools copyProperty(String sourcePropertyName, String destinationPropertyName) {
+        Object value = sourceWrapper.getPropertyValue(sourcePropertyName);
+        destinationWrapper.setPropertyValue(destinationPropertyName, value);
+        return this;
+    }
+
+    public Object getDestination() {
+        return destinationWrapper.getWrappedInstance();
+    }
+
+    public Object getSource() {
+        return sourceWrapper.getWrappedInstance();
+    }
+
+    /**
+     * Get the property's value from the source.
+     *
+     * @param propertyName
+     *            the name of the property
+     * @param type
+     *            the type to return
+     * @return the value
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getSourceProperty(String propertyName, Class<T> type) {
+        Object value = sourceWrapper.getPropertyValue(propertyName);
+        if (value == null) {
+            return null;
+        }
+        return (T) value;
+    }
+
+    private void init(Object source, Object destination) {
+        AssertTools.assertNotNull(source, "The source cannot be null");
+        AssertTools.assertNotNull(destination, "The destination cannot be null");
+        this.sourceWrapper = new BeanWrapperImpl(source);
+        this.destinationWrapper = new BeanWrapperImpl(destination);
+    }
+
+    /**
+     * Set a value on the destination.
+     *
+     * @param propertyName
+     *            the name of the property
+     * @param value
+     *            the value to set
+     */
+    public void setDestinationProperty(String propertyName, Object value) {
+        destinationWrapper.setPropertyValue(propertyName, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public BeanPropertiesCopierTools updateCollection(Collection<?> sourceCollection, String destinationPropertyName) {
+        // Get or create the destination
+        Object destinationObject = destinationWrapper.getPropertyValue(destinationPropertyName);
+        boolean setDestination = destinationObject == null;
+        Set<Object> destinationSet = null;
+        List<Object> destinationList = null;
+        Class<?> destinationPropertyType;
+        if (setDestination) {
+            destinationPropertyType = destinationWrapper.getPropertyType(destinationPropertyName);
+            if (destinationPropertyType == List.class) {
+                destinationList = new ArrayList<>();
+                destinationObject = destinationList;
+            }
+            if (destinationPropertyType == Set.class) {
+                destinationSet = new HashSet<>();
+                destinationObject = destinationSet;
+            }
+        } else {
+            destinationPropertyType = destinationObject.getClass();
+
+            if (destinationObject instanceof List) {
+                destinationList = (List<Object>) destinationObject;
+            }
+            if (destinationObject instanceof Set) {
+                destinationSet = (Set<Object>) destinationObject;
+            }
+        }
+
+        // Check we have a valid destination
+        if (destinationList == null && destinationSet == null) {
+            throw new SmallToolsException("The destination must be a List or a Set. Is a " + destinationPropertyType.getName());
+        }
+
+        // Get the source
+        if (sourceCollection == null) {
+            sourceCollection = Collections.emptyList();
+        }
+
+        // Copy depending on the type
+        if (destinationList != null) {
+            int destPos = 0;
+            for (Object next : sourceCollection) {
+                // Skip or insert
+                if (destPos >= destinationList.size() || !destinationList.get(destPos).equals(next)) {
+                    destinationList.add(destPos, next);
+                }
+
+                ++destPos;
+            }
+            // Remove extra
+            while (destPos < destinationList.size()) {
+                destinationList.remove(destPos);
+            }
+
+        }
+        if (destinationSet != null) {
+            // Remove
+            Iterator<?> destinationIt = destinationSet.iterator();
+            while (destinationIt.hasNext()) {
+                Object next = destinationIt.next();
+                if (!sourceCollection.contains(next)) {
+                    destinationIt.remove();
+                }
+            }
+
+            // Add
+            for (Object next : sourceCollection) {
+                if (!destinationSet.contains(next)) {
+                    destinationSet.add(next);
+                }
+            }
+        }
+
+        // Set the destination
+        if (setDestination) {
+            destinationWrapper.setPropertyValue(destinationPropertyName, destinationObject);
+        }
+
+        return this;
+    }
+
+    /**
+     * Take or create the collection on the destination and update with the source. This is useful when the destination collection could be a wrapper of the collection to keep track of it (e.g when
+     * using Hibernate)
+     *
+     * @param propertyName
+     *            the name of the source's and destination's property
+     * @return this
+     */
+    public BeanPropertiesCopierTools updateCollection(String propertyName) {
+        return updateCollection(propertyName, propertyName);
+    }
+
+    /**
+     * Take or create the collection on the destination and update with the source. This is useful when the destination collection could be a wrapper of the collection to keep track of it (e.g when
+     * using Hibernate)
+     *
+     * @param sourcePropertyName
+     *            the name of the source's property
+     * @param destinationPropertyName
+     *            the name of the destination's property
+     * @return this
+     */
+    public BeanPropertiesCopierTools updateCollection(String sourcePropertyName, String destinationPropertyName) {
+        Collection<?> sourceCollection = (Collection<?>) sourceWrapper.getPropertyValue(sourcePropertyName);
+        return updateCollection(sourceCollection, destinationPropertyName);
+    }
+}
---
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/reflection/BeanPropertiesCopierToolsTest.java
@@ -0,0 +1,278 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2017 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.reflection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BeanPropertiesCopierToolsTest {
+
+    public static class DestinationBeanPropertiesCopierTools {
+        private String text;
+        private String secondText;
+        private int number;
+        private int secondNumber;
+        private Set<String> texts;
+        private Set<String> secondTexts;
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getSecondNumber() {
+            return secondNumber;
+        }
+
+        public String getSecondText() {
+            return secondText;
+        }
+
+        public Set<String> getSecondTexts() {
+            return secondTexts;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public Set<String> getTexts() {
+            return texts;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public void setSecondNumber(int secondNumber) {
+            this.secondNumber = secondNumber;
+        }
+
+        public void setSecondText(String secondText) {
+            this.secondText = secondText;
+        }
+
+        public void setSecondTexts(Set<String> secondTexts) {
+            this.secondTexts = secondTexts;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public void setTexts(Set<String> texts) {
+            this.texts = texts;
+        }
+
+    }
+
+    public static class SourceBeanPropertiesCopierTools {
+        private String text;
+        private int number;
+        private List<String> texts;
+        private List<String> secondTexts;
+
+        public int getNumber() {
+            return number;
+        }
+
+        public List<String> getSecondTexts() {
+            return secondTexts;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public List<String> getTexts() {
+            return texts;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public void setSecondTexts(List<String> secondTexts) {
+            this.secondTexts = secondTexts;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public void setTexts(List<String> texts) {
+            this.texts = texts;
+        }
+
+    }
+
+    private void assertCollectionNoOrder(Collection<String> collection, String... values) {
+        Assert.assertNotNull(collection);
+        for (String value : values) {
+            Assert.assertTrue(collection.contains(value));
+        }
+        Assert.assertEquals(values.length, collection.size());
+    }
+
+    private Set<String> newHashSet(String... values) {
+        Set<String> result = new HashSet<>();
+        for (String value : values) {
+            result.add(value);
+        }
+        return result;
+    }
+
+    @Test
+    public void testCopyProperty1() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setText("Hello World");
+        source.setNumber(5);
+        source.setTexts(Arrays.asList("One", "Two"));
+        DestinationBeanPropertiesCopierTools destination = new DestinationBeanPropertiesCopierTools();
+        destination.setNumber(10);
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.copyProperty("text");
+        copierTools.copyProperty("number");
+
+        Assert.assertEquals("Hello World", destination.getText());
+        Assert.assertEquals(5, destination.getNumber());
+    }
+
+    @Test
+    public void testCopyProperty1_List() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setTexts(Arrays.asList("One", "Two"));
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.copyProperty("texts");
+
+        Assert.assertEquals(Arrays.asList("One", "Two"), destination.getTexts());
+    }
+
+    @Test
+    public void testCopyProperty1_Null() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+        destination.setText("Hello World");
+        destination.setTexts(Arrays.asList("One", "Two"));
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.copyProperty("text");
+        copierTools.copyProperty("texts");
+
+        Assert.assertNull(destination.getText());
+        Assert.assertNull(destination.getTexts());
+    }
+
+    @Test
+    public void testCopyProperty2() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setText("Hello World");
+        source.setNumber(5);
+        source.setTexts(Arrays.asList("One", "Two"));
+        DestinationBeanPropertiesCopierTools destination = new DestinationBeanPropertiesCopierTools();
+        destination.setNumber(10);
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.copyProperty("text", "secondText");
+        copierTools.copyProperty("number", "secondNumber");
+
+        Assert.assertNull(destination.getText());
+        Assert.assertEquals("Hello World", destination.getSecondText());
+        Assert.assertEquals(10, destination.getNumber());
+        Assert.assertEquals(5, destination.getSecondNumber());
+    }
+
+    @Test
+    public void testCopyProperty2_List() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setTexts(Arrays.asList("One", "Two"));
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.copyProperty("texts", "secondTexts");
+
+        Assert.assertNull(destination.getTexts());
+        Assert.assertEquals(Arrays.asList("One", "Two"), destination.getSecondTexts());
+    }
+
+    @Test
+    public void testUpdateCollection1ListToList() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setTexts(Arrays.asList("AAA", "BBB", "CCC"));
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+        destination.setTexts(new ArrayList<>(Arrays.asList("EEE", "BBB")));
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.updateCollection("texts");
+
+        Assert.assertEquals(Arrays.asList("AAA", "BBB", "CCC"), destination.getTexts());
+    }
+
+    @Test
+    public void testUpdateCollection1ListToSet() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setTexts(Arrays.asList("AAA", "BBB", "CCC"));
+        DestinationBeanPropertiesCopierTools destination = new DestinationBeanPropertiesCopierTools();
+        destination.setTexts(newHashSet("EEE", "BBB"));
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.updateCollection("texts");
+
+        assertCollectionNoOrder(destination.getTexts(), "AAA", "BBB", "CCC");
+    }
+
+    @Test
+    public void testUpdateCollection1SetToList() {
+        DestinationBeanPropertiesCopierTools source = new DestinationBeanPropertiesCopierTools();
+        source.setTexts(newHashSet("AAA", "BBB", "CCC"));
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+        destination.setTexts(new ArrayList<>(Arrays.asList("EEE", "BBB")));
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.updateCollection("texts");
+
+        assertCollectionNoOrder(destination.getTexts(), "AAA", "BBB", "CCC");
+    }
+
+    @Test
+    public void testUpdateCollection1SourceListNull() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        SourceBeanPropertiesCopierTools destination = new SourceBeanPropertiesCopierTools();
+        destination.setTexts(new ArrayList<>(Arrays.asList("EEE", "BBB")));
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.updateCollection("texts");
+
+        Assert.assertEquals(Arrays.asList(), destination.getTexts());
+    }
+
+    @Test
+    public void testUpdateCollection2() {
+        SourceBeanPropertiesCopierTools source = new SourceBeanPropertiesCopierTools();
+        source.setTexts(Arrays.asList("AAA", "BBB", "CCC"));
+        DestinationBeanPropertiesCopierTools destination = new DestinationBeanPropertiesCopierTools();
+
+        BeanPropertiesCopierTools copierTools = new BeanPropertiesCopierTools(source, destination);
+        copierTools.updateCollection("texts", "secondTexts");
+
+        Assert.assertNull(destination.getTexts());
+        assertCollectionNoOrder(destination.getSecondTexts(), "AAA", "BBB", "CCC");
+
+    }
+
+}
