90fa613d9ae0e4e9c0b2edcdf6c6bd644e9b6f0d
Simon Levesque <simon@simonlevesque.com>
2017-11-13 16:23:17 -0500

    SystemTools - Add getPropertyOrEnvironment

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/SystemTools.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/SystemTools.java
@@ -11,6 +11,40 @@ package com.foilen.smalltools.tools;
 public class SystemTools {
 
     /**
+     * Get the property, the environment or null value.
+     *
+     * @param key
+     *            the key
+     * @return the value
+     */
+    public static String getPropertyOrEnvironment(String key) {
+        return getPropertyOrEnvironment(key, null);
+    }
+
+    /**
+     * Get the property, the environment or the default value.
+     *
+     * @param key
+     *            the key
+     * @param defaultValue
+     *            the default value
+     * @return the value
+     */
+    public static String getPropertyOrEnvironment(String key, String defaultValue) {
+        String value = System.getProperty(key);
+        if (value != null) {
+            return value;
+        }
+
+        value = System.getenv(key);
+        if (value != null) {
+            return value;
+        }
+
+        return defaultValue;
+    }
+
+    /**
      * Sets a system property if not already defined.
      *
      * @param key
