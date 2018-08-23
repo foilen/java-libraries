4ef22507ea030ae31365e91d913980cd9c6b8ba3
Simon Levesque <simon@foilen.com>
2018-08-23 18:29:26 -0400

    StringTools - Add safeComparisonNullFirst and safeComparisonNullLast

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/StringTools.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/StringTools.java
@@ -11,6 +11,52 @@ package com.foilen.smalltools.tools;
 public class StringTools {
 
     /**
+     * Compare both Strings, not crashing for nulls..
+     *
+     * @param first
+     *            the first string
+     * @param second
+     *            the second string
+     * @return the comparison result
+     */
+    public static int safeComparisonNullFirst(String first, String second) {
+        if (first == null) {
+            if (second == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        if (second == null) {
+            return 1;
+        }
+        return first.compareTo(second);
+    }
+
+    /**
+     * Compare both Strings, not crashing for nulls..
+     *
+     * @param first
+     *            the first string
+     * @param second
+     *            the second string
+     * @return the comparison result
+     */
+    public static int safeComparisonNullLast(String first, String second) {
+        if (first == null) {
+            if (second == null) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        if (second == null) {
+            return -1;
+        }
+        return first.compareTo(second);
+    }
+
+    /**
      * Tells if both are equals, not crashing for nulls.
      *
      * @param first
--- jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/StringToolsTest.java
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/StringToolsTest.java
@@ -14,6 +14,26 @@ import org.junit.Test;
 public class StringToolsTest {
 
     @Test
+    public void testSafeComparisonNullFirst() {
+        Assert.assertTrue(StringTools.safeComparisonNullFirst("a", "a") == 0);
+        Assert.assertTrue(StringTools.safeComparisonNullFirst("a", "b") < 0);
+        Assert.assertTrue(StringTools.safeComparisonNullFirst("b", "a") > 0);
+        Assert.assertTrue(StringTools.safeComparisonNullFirst(null, "b") < 0);
+        Assert.assertTrue(StringTools.safeComparisonNullFirst("b", null) > 0);
+        Assert.assertTrue(StringTools.safeComparisonNullFirst(null, null) == 0);
+    }
+
+    @Test
+    public void testSafeComparisonNullLast() {
+        Assert.assertTrue(StringTools.safeComparisonNullLast("a", "a") == 0);
+        Assert.assertTrue(StringTools.safeComparisonNullLast("a", "b") < 0);
+        Assert.assertTrue(StringTools.safeComparisonNullLast("b", "a") > 0);
+        Assert.assertTrue(StringTools.safeComparisonNullLast(null, "b") > 0);
+        Assert.assertTrue(StringTools.safeComparisonNullLast("b", null) < 0);
+        Assert.assertTrue(StringTools.safeComparisonNullLast(null, null) == 0);
+    }
+
+    @Test
     public void testSafeEquals() {
         Assert.assertTrue(StringTools.safeEquals(null, null));
         Assert.assertFalse(StringTools.safeEquals("a", null));
