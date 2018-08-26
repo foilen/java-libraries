6fd517fa344b9b5ef8caaf10ef018bb01f5a531e
Simon Levesque <simon@foilen.com>
2018-08-26 11:13:01 -0400

    Add TimeExecutionTools that can measure the time it takes to execute something

---
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/TimeExecutionTools.java
@@ -0,0 +1,27 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.tools;
+
+public class TimeExecutionTools extends AbstractBasics {
+
+    /**
+     * Measure the execution time in ms
+     * 
+     * @param runnable
+     *            what to execute
+     * @return the execution time in ms
+     */
+    public static long measureInMs(Runnable runnable) {
+        long start = System.currentTimeMillis();
+        runnable.run();
+        return System.currentTimeMillis() - start;
+
+    }
+
+}
