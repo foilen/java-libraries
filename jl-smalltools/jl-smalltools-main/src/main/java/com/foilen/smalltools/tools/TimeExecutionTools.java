20ca0dd006b6e01ae7bdee03ac35971e8b8b22d8
Simon Levesque <simon@foilen.com>
2018-08-26 12:38:37 -0400

    Javadoc format

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/TimeExecutionTools.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/TimeExecutionTools.java
@@ -12,7 +12,7 @@ public class TimeExecutionTools extends AbstractBasics {
 
     /**
      * Measure the execution time in ms
-     * 
+     *
      * @param runnable
      *            what to execute
      * @return the execution time in ms
