74e145994d90592902c956a73a5c4f820e85a86e
Simon Levesque <simon@foilen.com>
2018-08-28 07:19:28 -0400

    Rest API - Order fields alphabetically

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/restapi/model/AbstractApiBase.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/restapi/model/AbstractApiBase.java
@@ -11,6 +11,7 @@ package com.foilen.smalltools.restapi.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.foilen.smalltools.tools.AbstractBasics;
 
 /**
@@ -25,6 +26,7 @@ import com.foilen.smalltools.tools.AbstractBasics;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(value = Include.NON_EMPTY, content = Include.NON_NULL)
+@JsonPropertyOrder(alphabetic = true)
 public abstract class AbstractApiBase extends AbstractBasics {
 
 }
