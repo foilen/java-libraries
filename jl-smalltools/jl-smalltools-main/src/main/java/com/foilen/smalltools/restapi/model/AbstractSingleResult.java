511cd68e6a3a4c1fe55603494e3cfa8159d5c111
Simon Levesque <simon@foilen.com>
2018-08-28 07:06:35 -0400

    Rest API - Add AbstractSingleResult

---
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/restapi/model/AbstractSingleResult.java
@@ -0,0 +1,36 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.restapi.model;
+
+/**
+ * To provide a single item. You need to extends to enable deserialization on the client side.
+ *
+ * <pre>
+ * Dependencies:
+ * compile 'org.apache.commons:commons-lang3:3.6'
+ * compile 'com.fasterxml.jackson.core:jackson-databind:2.9.1'
+ * compile 'org.slf4j:slf4j-api:1.7.25'
+ * </pre>
+ *
+ * @param <T>
+ *            the type of the returned item
+ */
+public abstract class AbstractSingleResult<T> extends AbstractApiBaseWithError {
+
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+}
