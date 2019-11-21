a79c6f80d0cbae2fb08ea38a2befb7fbad18e331
Simon Levesque <simon@foilen.com>
2019-11-20 21:10:38 -0500

    Add PriceFormatTools

---
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/PriceFormatTools.java
@@ -0,0 +1,97 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2019 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.tools;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+import com.google.common.base.Strings;
+
+/**
+ * To ensure correct decimal value, you can use a long to store the price. For a price like AAA.BB, you would store in a long the value AAABB.
+ */
+public class PriceFormatTools {
+
+    /**
+     * Convert a long in the form AAABB to a text AAA.BB .
+     * 
+     * @param price
+     *            the price
+     * @return the formatted price
+     */
+    public static String toDigit(long price) {
+        long a = price / 100;
+        long b = Math.abs(price % 100);
+
+        String decimal = String.valueOf(b);
+        if (decimal.length() == 1) {
+            decimal = "0" + decimal;
+        }
+
+        return a + "." + decimal;
+    }
+
+    /**
+     * Given a price in the format AAA.BB , convert it to a long value of AAABB .
+     * 
+     * @param text
+     *            the text
+     * @return the long
+     */
+    public static long toLong(String text) {
+
+        if (Strings.isNullOrEmpty(text)) {
+            return 0;
+        }
+
+        int dotPosition = text.indexOf(".");
+        int comaPosition = text.indexOf(",");
+        if (dotPosition != -1 && comaPosition != -1) {
+            throw new SmallToolsException("Wrong price format");
+        }
+
+        int pos = Math.max(comaPosition, dotPosition);
+
+        boolean hasDot = pos != -1;
+        if (!hasDot) {
+            pos = text.length();
+        }
+
+        // Get the parts
+        String first = text.substring(0, pos);
+        String second = "";
+        if (hasDot) {
+            second = text.substring(pos + 1);
+            if (second.length() > 2) {
+                throw new SmallToolsException("Wrong price format");
+            }
+        }
+        long a = 0;
+        try {
+            if (!Strings.isNullOrEmpty(first)) {
+                a = Long.valueOf(first);
+            }
+        } catch (Exception e) {
+            throw new SmallToolsException("Wrong price format");
+        }
+
+        long b = 0;
+        try {
+            if (!Strings.isNullOrEmpty(second)) {
+                while (second.length() < 2) {
+                    second += '0';
+                }
+
+                b = Long.valueOf(second);
+            }
+        } catch (Exception e) {
+            throw new SmallToolsException("Wrong price format");
+        }
+
+        return a * 100 + b;
+    }
+}
---
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/PriceFormatToolsTest.java
@@ -0,0 +1,82 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2019 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.tools;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.foilen.smalltools.exception.SmallToolsException;
+
+public class PriceFormatToolsTest {
+
+    @Test
+    public void testToDigit() {
+        Assert.assertEquals("0.00", PriceFormatTools.toDigit(0));
+        Assert.assertEquals("0.10", PriceFormatTools.toDigit(10));
+        Assert.assertEquals("1.20", PriceFormatTools.toDigit(120));
+        Assert.assertEquals("12.34", PriceFormatTools.toDigit(1234));
+        Assert.assertEquals("1234.56", PriceFormatTools.toDigit(123456));
+    }
+
+    @Test
+    public void testToLong() {
+        Assert.assertEquals(0, PriceFormatTools.toLong("0.00"));
+        Assert.assertEquals(0, PriceFormatTools.toLong("0.0"));
+        Assert.assertEquals(0, PriceFormatTools.toLong("0."));
+        Assert.assertEquals(0, PriceFormatTools.toLong("."));
+        Assert.assertEquals(0, PriceFormatTools.toLong("0"));
+        Assert.assertEquals(0, PriceFormatTools.toLong(""));
+        Assert.assertEquals(0, PriceFormatTools.toLong(null));
+        Assert.assertEquals(10, PriceFormatTools.toLong("0.10"));
+        Assert.assertEquals(10, PriceFormatTools.toLong("0.1"));
+        Assert.assertEquals(120, PriceFormatTools.toLong("1.20"));
+        Assert.assertEquals(1234, PriceFormatTools.toLong("12.34"));
+        Assert.assertEquals(123456, PriceFormatTools.toLong("1234.56"));
+        Assert.assertEquals(123456, PriceFormatTools.toLong("1234,56"));
+        Assert.assertEquals(123450, PriceFormatTools.toLong("1234.5"));
+        Assert.assertEquals(123400, PriceFormatTools.toLong("1234"));
+        Assert.assertEquals(123400, PriceFormatTools.toLong("1234."));
+        Assert.assertEquals(123400, PriceFormatTools.toLong("1234,"));
+    }
+
+    @Test
+    public void testToLongInvalid() {
+        try {
+            PriceFormatTools.toLong("0.00.0");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+        try {
+            PriceFormatTools.toLong("0.00,0");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+        try {
+            PriceFormatTools.toLong("0-00");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+        try {
+            PriceFormatTools.toLong("1.234");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+        try {
+            PriceFormatTools.toLong("1.c4");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+        try {
+            PriceFormatTools.toLong("1carotte4");
+            Assert.fail("Expecting failure");
+        } catch (SmallToolsException e) {
+        }
+    }
+
+}
