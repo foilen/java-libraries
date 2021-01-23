36fc2a1d403df55ea5305d1c954ec67af44b92ef
Simon Levesque <simon@foilen.com>
2021-01-23 11:33:26 -0500

    TimeConverterTools - Add methods for "from seconds" and "from minutes"

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/TimeConverterTools.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/TimeConverterTools.java
@@ -25,8 +25,50 @@ public class TimeConverterTools {
      * @param totalInMs
      *            the time to convert
      * @return the time like "24s 34ms" or "34w 5d 16h 1m 15s 856ms"
+     * @deprecated use {@link #convertToTextFromMs(Long)}
      */
+    @Deprecated
     static public String convertToText(Long totalInMs) {
+        return convertToTextFromMs(totalInMs);
+    }
+
+    /**
+     * Take an amount of minutes and transform them to text up to weeks.
+     *
+     * @param totalInMin
+     *            the time to convert
+     * @return the time like "24s" or "34w 5d 16h 1m"
+     */
+    static public String convertToTextFromMin(Long totalInMin) {
+
+        StringBuilder text = new StringBuilder();
+
+        if (totalInMin == null) {
+            return null;
+        }
+
+        long totalInMs = totalInMin * 60000;
+        totalInMs = convertToTextTakeSome(totalInMs, WEEK, "w", text);
+        totalInMs = convertToTextTakeSome(totalInMs, DAY, "d", text);
+        totalInMs = convertToTextTakeSome(totalInMs, HOUR, "h", text);
+        totalInMs = convertToTextTakeSome(totalInMs, MINUTE, "m", text);
+
+        if (text.length() == 0) {
+            return "0m";
+        }
+
+        return text.toString();
+
+    }
+
+    /**
+     * Take an amount of milliseconds and transform them to text up to weeks.
+     *
+     * @param totalInMs
+     *            the time to convert
+     * @return the time like "24s 34ms" or "34w 5d 16h 1m 15s 856ms"
+     */
+    static public String convertToTextFromMs(Long totalInMs) {
 
         StringBuilder text = new StringBuilder();
 
@@ -43,6 +85,36 @@ public class TimeConverterTools {
 
         if (text.length() == 0) {
             return "0ms";
+        }
+
+        return text.toString();
+
+    }
+
+    /**
+     * Take an amount of seconds and transform them to text up to weeks.
+     *
+     * @param totalInSec
+     *            the time to convert
+     * @return the time like "24s" or "34w 5d 16h 1m 15s"
+     */
+    static public String convertToTextFromSec(Long totalInSec) {
+
+        StringBuilder text = new StringBuilder();
+
+        if (totalInSec == null) {
+            return null;
+        }
+
+        long totalInMs = totalInSec * 1000;
+        totalInMs = convertToTextTakeSome(totalInMs, WEEK, "w", text);
+        totalInMs = convertToTextTakeSome(totalInMs, DAY, "d", text);
+        totalInMs = convertToTextTakeSome(totalInMs, HOUR, "h", text);
+        totalInMs = convertToTextTakeSome(totalInMs, MINUTE, "m", text);
+        totalInMs = convertToTextTakeSome(totalInMs, SECOND, "s", text);
+
+        if (text.length() == 0) {
+            return "0s";
         }
 
         return text.toString();
--- jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/TimeConverterToolsTest.java
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/TimeConverterToolsTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 public class TimeConverterToolsTest {
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testConvertToText() {
         Assert.assertEquals(null, TimeConverterTools.convertToText(null));
@@ -23,6 +24,36 @@ public class TimeConverterToolsTest {
         Assert.assertEquals("24s 34ms", TimeConverterTools.convertToText(24034L));
         Assert.assertEquals("1m 15s 856ms", TimeConverterTools.convertToText(75856L));
         Assert.assertEquals("34w 5d 16h 1m 15s 856ms", TimeConverterTools.convertToText(21052875856L));
+    }
+
+    @Test
+    public void testConvertToTextFromMin() {
+        Assert.assertEquals(null, TimeConverterTools.convertToTextFromMin(null));
+        Assert.assertEquals("0m", TimeConverterTools.convertToTextFromMin(0L));
+        Assert.assertEquals("24m", TimeConverterTools.convertToTextFromMin(24L));
+        Assert.assertEquals("1h 15m", TimeConverterTools.convertToTextFromMin(75L));
+        Assert.assertEquals("34w 5d 16h 1m", TimeConverterTools.convertToTextFromMin(350881L));
+    }
+
+    @Test
+    public void testConvertToTextFromMs() {
+        Assert.assertEquals(null, TimeConverterTools.convertToTextFromMs(null));
+        Assert.assertEquals("0ms", TimeConverterTools.convertToTextFromMs(0L));
+        Assert.assertEquals("10ms", TimeConverterTools.convertToTextFromMs(10L));
+        Assert.assertEquals("250ms", TimeConverterTools.convertToTextFromMs(250L));
+        Assert.assertEquals("24s 0ms", TimeConverterTools.convertToTextFromMs(24000L));
+        Assert.assertEquals("24s 34ms", TimeConverterTools.convertToTextFromMs(24034L));
+        Assert.assertEquals("1m 15s 856ms", TimeConverterTools.convertToTextFromMs(75856L));
+        Assert.assertEquals("34w 5d 16h 1m 15s 856ms", TimeConverterTools.convertToTextFromMs(21052875856L));
+    }
+
+    @Test
+    public void testConvertToTextFromSec() {
+        Assert.assertEquals(null, TimeConverterTools.convertToTextFromSec(null));
+        Assert.assertEquals("0s", TimeConverterTools.convertToTextFromSec(0L));
+        Assert.assertEquals("24s", TimeConverterTools.convertToTextFromSec(24L));
+        Assert.assertEquals("1m 15s", TimeConverterTools.convertToTextFromSec(75L));
+        Assert.assertEquals("34w 5d 16h 1m 15s", TimeConverterTools.convertToTextFromSec(21052875L));
     }
 
 }
