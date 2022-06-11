996b90c41064537fa3ad04bd7ef0b12b71a0eaa9
Simon Levesque <simon@foilen.com>
2022-06-10 21:43:25 -0400

    Update copyright

--- jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/DirectoryToolsTest.java
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/DirectoryToolsTest.java
@@ -1,6 +1,6 @@
 /*
     Java Libraries https://github.com/foilen/java-libraries
-    Copyright (c) 2015-2021 Foilen (https://foilen.com)
+    Copyright (c) 2015-2022 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
