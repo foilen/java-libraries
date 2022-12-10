74766f95f97a80b3343642caf4d7a3b4ec8bea00
Simon Levesque <simon@foilen.com>
2022-12-10 03:32:12 +0000

    Add AbstractApiBaseWithAdditionalProperties

---
+++ jl-smalltools/jl-smalltools-spring/src/main/java/com/foilen/smalltools/restapi/model/AbstractApiBaseWithAdditionalProperties.java
@@ -0,0 +1,27 @@
+package com.foilen.smalltools.restapi.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Like {@link AbstractApiBase}, but all the unknown properties are going in a Map.
+ */
+public class AbstractApiBaseWithAdditionalProperties extends AbstractApiBase {
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperties(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
