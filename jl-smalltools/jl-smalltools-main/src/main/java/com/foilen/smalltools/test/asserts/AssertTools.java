625cb7ba44f3da7cebaed8117e230d29095bf934
Simon Levesque <simon@foilen.com>
2023-05-24 21:43:54 -0400

    ASSERT_TOOLS_UPDATE_EXPECTED_FILE Support filtering the path when many potential files

--- jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/test/asserts/AssertTools.java
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/test/asserts/AssertTools.java
@@ -8,11 +8,6 @@
  */
 package com.foilen.smalltools.test.asserts;
 
-import com.foilen.smalltools.JavaEnvironmentValues;
-import com.foilen.smalltools.exception.SmallToolsException;
-import com.foilen.smalltools.tools.*;
-import org.junit.Assert;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -20,6 +15,17 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.junit.Assert;
+
+import com.foilen.smalltools.JavaEnvironmentValues;
+import com.foilen.smalltools.exception.SmallToolsException;
+import com.foilen.smalltools.tools.CollectionsTools;
+import com.foilen.smalltools.tools.DirectoryTools;
+import com.foilen.smalltools.tools.FileTools;
+import com.foilen.smalltools.tools.JsonTools;
+import com.foilen.smalltools.tools.ResourceTools;
+import com.foilen.smalltools.tools.SystemTools;
 
 /**
  * Assertions.
@@ -39,9 +45,9 @@ public final class AssertTools {
 
     /**
      * Load an expected object from a JSON resource file and diff the initial vs final items (by comparing their JSON dump) and output as JSON dump (by their JSON dump).
-     * <br>
-     * <br>
-     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" to let the tool update the file with the "actual" json.
+     * <p>
+     * <p>
+     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" or some part of the path to let the tool update the file with the "actual" json.
      *
      * @param expectedResource the filename of the resource
      * @param expectedContext  the class in which the resource file is
@@ -65,8 +71,8 @@ public final class AssertTools {
 
     /**
      * Load an expected object from a JSON resource file and diff the initial vs final items (by comparing their JSON dump) and output as JSON dump (by their JSON dump ignoring nulls).
-     * <br>
-     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" to let the tool update the file with the "actual" json.
+     * <p>
+     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" or some part of the path to let the tool update the file with the "actual" json.
      *
      * @param expectedResource the filename of the resource
      * @param expectedContext  the class in which the resource file is
@@ -166,9 +172,9 @@ public final class AssertTools {
 
     /**
      * Load an expected object from a JSON resource file and compare it to the actual object (by their JSON dump).
-     * <br>
-     * <br>
-     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" to let the tool update the file with the "actual" json.
+     * <p>
+     * <p>
+     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" or some part of the path to let the tool update the file with the "actual" json.
      *
      * @param expectedResource the filename of the resource
      * @param expectedContext  the class in which the resource file is
@@ -198,8 +204,8 @@ public final class AssertTools {
 
     /**
      * Load an expected object from a JSON resource file and compare it to the actual object (by their JSON dump ignoring nulls).
-     * <br>
-     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" to let the tool update the file with the "actual" json.
+     * <p>
+     * You can set the system property "ASSERT_TOOLS_UPDATE_EXPECTED_FILE" to "true" or some part of the path to let the tool update the file with the "actual" json.
      *
      * @param expectedResource the filename of the resource
      * @param expectedContext  the class in which the resource file is
@@ -259,11 +265,11 @@ public final class AssertTools {
     private static AssertDiff diffLists(List<?> initialItems, List<?> finalItems) {
 
         // Transform items to json
-        List<String> initialJson = initialItems.stream() //
-                .map(i -> JsonTools.prettyPrint(i)) //
+        List<String> initialJson = initialItems.stream()
+                .map(JsonTools::prettyPrint)
                 .collect(CollectionsTools.collectToArrayList());
-        List<String> finalJson = finalItems.stream() //
-                .map(i -> JsonTools.prettyPrint(i)) //
+        List<String> finalJson = finalItems.stream()
+                .map(JsonTools::prettyPrint)
                 .collect(CollectionsTools.collectToArrayList());
 
         // Diff
@@ -292,19 +298,31 @@ public final class AssertTools {
      * @return true if should assert (when no need to update the resource file)
      */
     private static boolean updateFileIfRequested(String expectedResource, Class<?> expectedContext, String actualJson) {
-        if ("true".equals(SystemTools.getPropertyOrEnvironment("ASSERT_TOOLS_UPDATE_EXPECTED_FILE", "false"))) {
+        String updateExpectedFileContains = SystemTools.getPropertyOrEnvironment("ASSERT_TOOLS_UPDATE_EXPECTED_FILE", "false");
+        if (!"false".equals(updateExpectedFileContains)) {
             URL url = expectedContext.getResource(expectedResource);
             if (url == null) {
                 Assert.fail("The file must already exists (you can create an empty file)");
             } else {
                 String filename = url.toString().substring(5);
                 String filePart = new File(filename).getName();
-                List<String> availableFiles = DirectoryTools.listFilesAndFoldersRecursively(JavaEnvironmentValues.getWorkingDirectory(), true).stream() //
-                        .filter(it -> it.endsWith(filePart) && !it.equals(filename)) //
-                        .collect(Collectors.toList());
+                List<String> availableFiles = DirectoryTools.listFilesAndFoldersRecursively(JavaEnvironmentValues.getWorkingDirectory(), true).stream()
+                        .filter(it -> it.endsWith(filePart) && !it.equals(filename))
+                        .filter(it -> it.contains(updateExpectedFileContains))
+                        .toList();
 
-                Assert.assertEquals("Must have exactly one candidate", 1, availableFiles.size());
-                FileTools.writeFile(actualJson, availableFiles.get(0));
+                if (availableFiles.isEmpty()) {
+                    Assert.fail("No file found for " + filePart);
+                }
+                if (availableFiles.size() > 1) {
+                    System.out.println("Too many files found for " + filePart + ":");
+                    availableFiles.forEach(System.out::println);
+                    System.out.println("You can configure ASSERT_TOOLS_UPDATE_EXPECTED_FILE to filter the path containing that value");
+                    Assert.fail("Too many files found for " + filePart);
+                }
+                String fileToUpdate = availableFiles.get(0);
+                System.out.println("Updating " + fileToUpdate);
+                FileTools.writeFile(actualJson, fileToUpdate);
             }
             return false;
         }
