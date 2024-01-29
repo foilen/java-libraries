fd9c4fb033173335764b6236786243ec72734fbe
Simon Levesque <simon@foilen.com>
2024-01-28 22:07:26 -0500

    Add FrequencyConverterTools

---
+++ jl-smalltools/jl-smalltools-main/src/main/java/com/foilen/smalltools/tools/FrequencyConverterTools.java
@@ -0,0 +1,68 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2023 Foilen (https://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.tools;
+
+/**
+ * To help converting frequency values to Long.
+ */
+public class FrequencyConverterTools {
+
+    static public long K = 1000L;
+    static public long M = 1000 * K;
+    static public long G = 1000 * M;
+    static public long T = 1000 * G;
+
+    /**
+     * Convert the value to its biggest unit with 2 decimals. (e.g: 123000 will become 123Khz ; 1230000 will become 1.23Mhz)
+     *
+     * @param hertz the amount of hertz
+     * @return the value with its unit
+     */
+    static public String convertToBiggestHzUnit(Long hertz) {
+        if (hertz == null) {
+            return null;
+        }
+
+        String unit = "Hz";
+        double main = hertz;
+
+        if (hertz >= T) {
+            unit = "Thz";
+            main = main / T;
+        } else if (hertz >= G) {
+            unit = "Ghz";
+            main = main / G;
+        } else if (hertz >= M) {
+            unit = "Mhz";
+            main = main / M;
+        } else if (hertz >= K) {
+            unit = "Khz";
+            main = main / K;
+        }
+
+        main *= 100;
+        main = Math.round(main) / 100.0;
+
+        String text = String.valueOf(main);
+        int dotPos = text.indexOf('.');
+        if (dotPos != -1) {
+            int maxDecimalPos = Math.min(text.length(), dotPos + 3);
+            while (text.charAt(maxDecimalPos - 1) == '.' || text.charAt(maxDecimalPos - 1) == '0') {
+                --maxDecimalPos;
+                if (text.charAt(maxDecimalPos) == '.') {
+                    break;
+                }
+            }
+            text = text.substring(0, maxDecimalPos);
+        }
+
+        return text + unit;
+    }
+
+}
---
+++ jl-smalltools/jl-smalltools-main/src/test/java/com/foilen/smalltools/tools/FrequencyConverterToolsTest.java
@@ -0,0 +1,60 @@
+/*
+    Java Libraries https://github.com/foilen/java-libraries
+    Copyright (c) 2015-2023 Foilen (https://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.smalltools.tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class FrequencyConverterToolsTest {
+
+    @Test
+    public void testConvertToBiggestHzUnit_NullInput_ReturnsNull() {
+        Long input = null;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertNull(result);
+    }
+
+    @Test
+    public void testConvertToBiggestHzUnit_HzInput_ReturnsHz() {
+        Long input = 500L;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertEquals("500Hz", result);
+    }
+
+    @Test
+    public void testConvertToBiggestHzUnit_KhzInput_ReturnsKhz() {
+        Long input = 1500L;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertEquals("1.5Khz", result);
+    }
+
+    @Test
+    public void testConvertToBiggestHzUnit_MhzInput_ReturnsMhz() {
+        Long input = 1500000L;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertEquals("1.5Mhz", result);
+    }
+
+    @Test
+    public void testConvertToBiggestHzUnit_GhzInput_ReturnsGhz() {
+        Long input = 1500000000L;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertEquals("1.5Ghz", result);
+    }
+
+    @Test
+    public void testConvertToBiggestHzUnit_ThzInput_ReturnsThz() {
+        Long input = 1500000000000L;
+        String result = FrequencyConverterTools.convertToBiggestHzUnit(input);
+        assertEquals("1.5Thz", result);
+    }
+
+}
