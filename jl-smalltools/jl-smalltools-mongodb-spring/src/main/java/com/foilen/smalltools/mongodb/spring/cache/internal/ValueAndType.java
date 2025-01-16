166875510e8349e3a1d6f6034d1afcb139fc3b9d
Simon Levesque <simon@foilen.com>
2025-01-16 10:50:20 -0500

    Update copyright

--- jl-smalltools/jl-smalltools-mongodb-spring/src/main/java/com/foilen/smalltools/mongodb/spring/cache/internal/ValueAndType.java
+++ jl-smalltools/jl-smalltools-mongodb-spring/src/main/java/com/foilen/smalltools/mongodb/spring/cache/internal/ValueAndType.java
@@ -1,6 +1,6 @@
 /*
     Java Libraries https://github.com/foilen/java-libraries
-    Copyright (c) 2015-2024 Foilen (https://foilen.com)
+    Copyright (c) 2015-2025 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
@@ -21,7 +21,7 @@ public class ValueAndType extends AbstractApiBase {
         if (this.type == null) {
             return null;
         }
-        
+
         Class<?> type = ReflectionTools.safelyGetClass(this.type);
         if (type == null) {
             throw new IllegalArgumentException("The type is unknown " + this.type);
